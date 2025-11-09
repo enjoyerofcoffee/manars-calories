874682bfbc3928c088f6fd82800b004c27061227
silvabacc <47926269+enjoyerofcoffee@users.noreply.github.com>
2025-11-09 15:22:57 +0000

    update settings

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanStackDevtools } from "@tanstack/react-devtools";
 
 import Header from "../components/Header";
 
